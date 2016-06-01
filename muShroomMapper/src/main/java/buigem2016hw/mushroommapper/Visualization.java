e55a8aa77984e39cda9083bcb752dd2058e07c71
aonanam <aonanam@bu.edu>
2016-06-01 10:32:15 -0400

    Cleanup of Visualization

--- muShroomMapper/src/main/java/buigem2016hw/mushroommapper/Visualization.java
+++ muShroomMapper/src/main/java/buigem2016hw/mushroommapper/Visualization.java
@@ -51,6 +51,5 @@ public class Visualization extends JApplet{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-        
     }
 }
