5d32761c20d37deaa7e1e263845dc9f8e0129272
aonanam <aonanam@bu.edu>
2016-06-13 15:42:25 -0400

    Added in in/out terminal variables to MuGates, edited constructors to appropriately set these variables

--- muShroomMapper/src/main/java/buigem2016hw/mushroommapper/MuGate.java
+++ muShroomMapper/src/main/java/buigem2016hw/mushroommapper/MuGate.java
@@ -5,6 +5,8 @@
  */
 package buigem2016hw.mushroommapper;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.cellocad.BU.dom.DGate;
 import org.json.JSONObject;
 
@@ -12,19 +14,28 @@ import org.json.JSONObject;
  *
  * @author Shane
  */
-public class MuGate extends DGate{
-    public JSONObject opInfo;
+public class MuGate extends DGate
+{
+    public JSONObject opInfo;   //from UCF, contains all info regarding the microfluidic operation the gate performs
     public String type;
     public String ioWire;
     public int io;
-    public String mintName;
-    
-    public MuGate(String type, String wireName){
+    public String mintName;         //name used for this gate in mint file
+    public int inTermInd;       //current index of inTerm JSONArray
+    public int outTermInd;      //current index of outTerm JSONArray
+    public int inTermVal = 4;       //by default input terminal is 4th orientation (left) EG: *** ----->IN[(Device)]OUT-----> ***
+    public int outTermVal = 2;      //by default output terminal is 2nd orientation (right)
+    public boolean inTermFlag = false;      //flag that is true if inTerm JSONArray exists
+    public boolean outTermFlag = false;     //flag that is true if outTerm JSONArray exists
+        
+    public MuGate(String type, String wireName)
+    {
         this.type = type;
         this.ioWire = wireName;
         this.symbol = "0";
     }
-    public MuGate(DGate dg, String type){
+    public MuGate(DGate dg, String type)
+    {
         symbol = dg.symbol;
         output = dg.output;
         input = dg.input;
@@ -32,10 +43,13 @@ public class MuGate extends DGate{
         gindex = dg.gindex;
         gatestage = dg.gatestage;
         picpath = dg.picpath;
+        inTermInd = 0;
+        outTermInd = 0;
         this.type=type;
     }
     
-    public void addOpInfo(JSONObject JSONInput){
+    public void addOpInfo(JSONObject JSONInput)
+    {
         opInfo = JSONInput;
     }
 }
