9b7f434b04d882794849f2fc8b46d45dfd649aca
aonanam <aonanam@bu.edu>
2016-07-19 09:50:14 -0400

    Translation overhaul (3/?)

--- muShroomMapper/src/main/java/buigem2016hw/mushroommapper/NetListTransition.java
+++ muShroomMapper/src/main/java/buigem2016hw/mushroommapper/NetListTransition.java
@@ -8,9 +8,7 @@ package buigem2016hw.mushroommapper;
 import java.util.ArrayList;
 import java.util.List;
 import org.cellocad.BU.dom.DGate;
-import org.cellocad.BU.dom.DGateType;
 import org.cellocad.BU.dom.DWire;
-import org.cellocad.BU.dom.DWireType;
 import org.cellocad.BU.dom.LayerType;
 import org.cellocad.BU.fluigi.VerilogFluigiGrammar;
 import org.cellocad.BU.fluigi.VerilogFluigiWalker;
@@ -27,24 +25,15 @@ public class NetListTransition
     public String filepath;
     public String line;
     VerilogFluigiWalker walker;
-    public List<String> inPorts = new ArrayList<>();
-    public List<String> outPorts = new ArrayList<>();
-    public List<MuWire> wires = new ArrayList<>();
-    public List<MuGate> gates = new ArrayList<>();
+    public List<DGate> gateGraph;
        
     public NetListTransition(ParsedUCF ucf, String vFilePath) throws JSONException
     {
         line = "";
         line = Utilities.getFileContentAsString(vFilePath);
         walker = VerilogFluigiGrammar.getuFWalker(line);
-        inPorts = walker.details.inputs;                //create list of in ports from inputList command
-        outPorts = walker.details.outputs;              //create list of out ports from outputList command
         int gateCount=0;           //counts the number of gates throughout for use with IDing gates for visualization
-//        for(DGate dg:walker.netlist)                    //translating all operation gates as MuGates
-//        {
-//            MuGate opGate = new MuGate(dg, "gate");
-//            gates.add(opGate);
-//        }
+        
         for(DGate dg:walker.netlist)
         {
             switch(dg.gtype)
@@ -56,6 +45,9 @@ public class NetListTransition
                     else if (dg.opInfo.getString("layer").equals("control")) dg.layer = LayerType.control;
                     else System.out.println("Operator " + dg.symbol + " without layer attribute! Check UCF!");
                     
+                    dg.inTermInd = 0;
+                    dg.outTermInd = 0;
+        
                     dg.inTermFlag = true;                   //these operation gates will have their in/outTerm JSONArrays defined in their opInfo
                     dg.outTermFlag = true;
                     
@@ -90,46 +82,31 @@ public class NetListTransition
                     break;       
             }
         }
-        
-//        for(String wireName:walker.details.inputs)      //translating all inputs as both input MuGates and connecting MuWires   TODO: Clean this up
+        gateGraph = walker.netlist;
+//        combineValveChannels();
+    }    
+//    public void combineValveChannels()      //identifying duplicate channels made by a valve splitting up a flow channel into input and output
+//    {
+//        for(MuGate gate:gates)
 //        {
-//            MuGate in = new MuGate("input", wireName);  //the MuGate for the inport
-//            MuWire w = new MuWire(wireName, 0, in);     //the MuWire coming out of the inport
-//
-//            wires.add(w);
-//            gates.add(in);
+//            int fInCount = 0;
+//            int cInCount = 0;
+//            for(DWire input:gate.input)
+//            {
+//                if(input.wtype.equals("fchan") || input.wtype.equals("finput")) fInCount++;
+//                else if (input.wtype.equals("cchan") || input.wtype.equals("cinput")) cInCount++;
+//            }
+//            if (fInCount==1 && cInCount==1)
+//            {
+//                for (MuWire input:gate.muInput)
+//                {
+//                    if(input.wtype.equals("fchan") || input.wtype.equals("finput")) //TODO: needs work
+//                    {
+//                        input.dupChannel = gate.muOutput;   //for identifying duplicate channel from valve
+//                        gate.muOutput.isWritten = true;
+//                    }
+//                }
+//            }
 //        }
-//        for(String wireName:walker.details.outputs)     //translating all outputs as both output MuGates and connecting MuWires TODO: Clean this up
-//        {
-//            MuGate out = new MuGate("output", wireName);//the MuGate for the outport
-//            wires.add(new MuWire(wireName, 1 , out));   //the MuWire off the outport
-//            gates.add(out);
-//        }
-        combineValveChannels();
-    }
-    
-    public void combineValveChannels()      //identifying duplicate channels made by a valve splitting up a flow channel into input and output
-    {
-        for(MuGate gate:gates)
-        {
-            int fInCount = 0;
-            int cInCount = 0;
-            for(DWire input:gate.input)
-            {
-                if(input.wtype.equals("fchan") || input.wtype.equals("finput")) fInCount++;
-                else if (input.wtype.equals("cchan") || input.wtype.equals("cinput")) cInCount++;
-            }
-            if (fInCount==1 && cInCount==1)
-            {
-                for (MuWire input:gate.muInput)
-                {
-                    if(input.wtype.equals("fchan") || input.wtype.equals("finput")) //TODO: needs work
-                    {
-                        input.dupChannel = gate.muOutput;   //for identifying duplicate channel from valve
-                        gate.muOutput.isWritten = true;
-                    }
-                }
-            }
-        }
-    }
+//    }
 }
