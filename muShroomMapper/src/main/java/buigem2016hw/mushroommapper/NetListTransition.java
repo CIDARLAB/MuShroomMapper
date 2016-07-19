519c1525002f38b770414ba2f8df13da40036e7a
aonanam <aonanam@bu.edu>
2016-07-19 09:21:49 -0400

    Translation overhault (2/?)

--- muShroomMapper/src/main/java/buigem2016hw/mushroommapper/NetListTransition.java
+++ muShroomMapper/src/main/java/buigem2016hw/mushroommapper/NetListTransition.java
@@ -70,6 +70,7 @@ public class NetListTransition
                     break;
                     
                 case uF_IN:
+                    dg.output.fromGate = dg;
                     
                     dg.outTermVal = 2;
                     dg.inTermVal = -1;                          //an input gate doesn't have an input from any other gate
@@ -79,95 +80,32 @@ public class NetListTransition
                     break;
                     
                 case uF_OUT:
+                    dg.input.get(0).toGate = dg;
                     
                     dg.inTermVal = 4;
                     dg.outTermVal = -1;                        //an output gate doesn't output to any other gate
                     
                     dg.gindex = gateCount;
                     gateCount++;
-                    break;
-                    
-                    
-                    
+                    break;       
             }
         }
-//        for(String wireName:walker.details.wires)       //translating all traditional verilog wires to MuWires
+        
+//        for(String wireName:walker.details.inputs)      //translating all inputs as both input MuGates and connecting MuWires   TODO: Clean this up
 //        {
-//            wires.add(new MuWire(wireName));
-//            System.out.println(wireName);
+//            MuGate in = new MuGate("input", wireName);  //the MuGate for the inport
+//            MuWire w = new MuWire(wireName, 0, in);     //the MuWire coming out of the inport
+//
+//            wires.add(w);
+//            gates.add(in);
 //        }
-        for(String wireName:walker.details.inputs)      //translating all inputs as both input MuGates and connecting MuWires   TODO: Clean this up
-        {
-            MuGate in = new MuGate("input", wireName);  //the MuGate for the inport
-            MuWire w = new MuWire(wireName, 0, in);     //the MuWire coming out of the inport
-//            in.outTermVal = 2;
-//            in.inTermVal = -1;                          //an input gate doesn't have an input from any other gate
-            wires.add(w);
-            gates.add(in);
-//            in.gindex = gateCount;
-//            gateCount++;
-            //System.out.println(wireName);
-        }
-        for(String wireName:walker.details.outputs)     //translating all outputs as both output MuGates and connecting MuWires TODO: Clean this up
-        {
-            MuGate out = new MuGate("output", wireName);//the MuGate for the outport
-            wires.add(new MuWire(wireName, 1 , out));   //the MuWire off the outport
-//            out.inTermVal = 4;
-//            out.outTermVal = -1;                        //an output gate doesn't output to any other gate
-            gates.add(out);
-//            out.gindex = gateCount;
-//            gateCount++;
-        }
-
-        parseNetList();
+//        for(String wireName:walker.details.outputs)     //translating all outputs as both output MuGates and connecting MuWires TODO: Clean this up
+//        {
+//            MuGate out = new MuGate("output", wireName);//the MuGate for the outport
+//            wires.add(new MuWire(wireName, 1 , out));   //the MuWire off the outport
+//            gates.add(out);
+//        }
         combineValveChannels();
-        setLayers();
-    }
-    public void parseNetList()      //TODO: could be cleaned up
-    {
-
-        //filling out MuWire objects by wirename
-        for(MuWire wire:wires)                                                      //goes through all wires
-        {
-            for(MuGate gate:gates)                                                  //goes through all gates
-            {
-                if ("input".equals(wire.type))                                      //if wire is an input wire
-                {
-                    for(DWire input:gate.input)                                     //for through all inputs of the gate
-                    {
-                        if(wire.name.equals(input.name))    //if input wire is input to the gate, set gate as wire's toGate
-                        {
-                            wire.setDestination(gate);
-                            wire.wtype = input.wtype;
-                        } 
-                    }
-                }
-                else if("output".equals(wire.type))                                 //if wire is an output wire
-                {
-                    if(wire.name.equals(gate.output.name))  //if output wire is output of the gate, set gate as wire's fromGate 
-                    {
-                        wire.setOrigin(gate);
-                        wire.wtype = gate.output.wtype;
-                    }   
-                }
-                else                                                                //if wire isn't an input/output wire
-                {  
-                    if(wire.name.equals(gate.output.name))  //if wire is output of gate, set gate as wire's fromGate
-                    {
-                        wire.setOrigin(gate);
-                        wire.wtype = gate.output.wtype;
-                    }    
-                    else for(DWire input:gate.input)                                //if not output of gate, go through all inputs of gate 
-                    {
-                        if(wire.name.equals(input.name))    //if wire is an input of the gate, set gate as wire's toGate
-                        {
-                            wire.setDestination(gate);
-                            wire.wtype = input.wtype;
-                        } 
-                    }
-                }
-            }
-        }      
     }
     
     public void combineValveChannels()      //identifying duplicate channels made by a valve splitting up a flow channel into input and output
@@ -191,44 +129,6 @@ public class NetListTransition
                         gate.muOutput.isWritten = true;
                     }
                 }
-            }
-        }
-    }
-    public void setLayers() throws JSONException
-    {
-        for (MuWire wire:wires)
-        {
-            DWireType dwt = wire.wtype;
-            switch(dwt)
-            {
-                case cinput:
-                case connector:         //temporary until f and c connector are implemented
-                //case cconnector:      //not yet implemented in NetSynth
-                    wire.layer = "control";
-                    break;
-                case finput:
-                //case fconnector:      //not yet implemented in NetSynth
-                    wire.layer = "flow";
-                    break;
-                default:
-                    System.out.println("Unidentified channel layer!");
-                    System.out.println("wire name: " + wire.name);
-                    break;
-            }          
-        }
-        for (MuGate gate:gates)
-        {
-            switch(gate.type)
-            {
-//                case "gate":
-//                    if (gate.opInfo.getString("layer").equals("flow")) gate.layer = "flow";
-//                    else if (gate.opInfo.getString("layer").equals("control")) gate.layer = "control";
-//                    else System.out.println("unidentified operational gate layer - check UCF?");
-//                    break;
-                case "input":
-                    if (gate.muOutput.type)
-                case "output":
-                    
             }
         }
     }
