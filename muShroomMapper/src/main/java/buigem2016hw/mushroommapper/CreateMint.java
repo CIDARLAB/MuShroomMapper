63fe424b846826ea330be46942045b3546f2c274
aonanam <aonanam@bu.edu>
2016-07-25 10:48:16 -0400

    Mint creation overhaul 6/6! Control layer implementation + restructure complete

--- muShroomMapper/src/main/java/buigem2016hw/mushroommapper/CreateMint.java
+++ muShroomMapper/src/main/java/buigem2016hw/mushroommapper/CreateMint.java
@@ -16,17 +16,17 @@ import org.json.JSONException;
 import org.json.JSONArray;
 
 /**
- *  Takes the modified netlist graph and the parsed UCF and creates a Mint file for use in Fluigi
+ * Takes the modified netlist graph and the parsed UCF and creates a Mint file
+ * for use in Fluigi
+ *
  * @author Shane
  */
-
 //To do: set up Mint control layer writing
-
-public class CreateMint 
-{
+public class CreateMint {
 //    String line = "";
     //initializing MINT sections to print
     //flow sections
+
     String flowInPorts = "";
     String flowOutPorts = "";
     String flowPorts = "";
@@ -38,7 +38,7 @@ public class CreateMint
     String controlPorts = "";
     String controlDevices = "";
     String controlChannels = "";
-    
+
     //initializing MINT section counters
     //flow counters
     int flowInPortCount = 0;
@@ -50,234 +50,217 @@ public class CreateMint
     int controlOutPortCount = 0;
     int controlDeviceCount = 0;
     int controlChannelCount = 0;
-    
+
     String portRadius = "100"; //to be connected with GUI
- 
+
 //    List<String> channelList;   
-    
-    public CreateMint(NetListTransition graph, ParsedUCF ucf, String fileName) throws UnsupportedEncodingException, FileNotFoundException, JSONException, IOException
-    {
-        for(DGate dg:graph.gateGraph)
-        {
-            switch(dg.gtype)
-            {
+    public CreateMint(NetListTransition graph, ParsedUCF ucf, String fileName, int channelWidth) throws UnsupportedEncodingException, FileNotFoundException, JSONException, IOException {
+        for (DGate dg : graph.gateGraph) {
+            switch (dg.gtype) {
                 case uF:
-                    switch (dg.layer) 
-                    {
+                    switch (dg.layer) {
                         case flow:
                             String fDevLine = dg.opInfo.getString("mint") + ";";
-                            fDevLine = fDevLine.replaceAll("NAME", "Device" + flowDeviceCount + dg.opInfo.getString("name"));
-                            dg.mintName = "Device" + flowDeviceCount + dg.opInfo.getString("name");
-                            
-                            for (DWire in:dg.input)
-                            {
-                                if (in.isWritten == false)
-                                {
-                                    
-                                    in.isWritten = true;
-                                }
-                            }
-                            
-                            if (dg.output.isWritten == false)                            
-                            {
-                                
-                                dg.output.isWritten = true;
-                            }
-                        
-                            flowDevices+=fDevLine;
-                            flowDevices+="\n";
+                            fDevLine = fDevLine.replaceAll("NAME", "fDevice" + flowDeviceCount + "_" + dg.opInfo.getString("name"));
+                            dg.mintName = "fDevice" + flowDeviceCount + "_" + dg.opInfo.getString("name");
+
+                            flowDevices += fDevLine;
+                            flowDevices += "\n";
                             dg.isWritten = true;
                             flowDeviceCount++;
                             break;
-                        
+
                         case control:       //TODO: test how control devices print
                             String cDevLine = dg.opInfo.getString("mint") + ";";
-                            cDevLine = cDevLine.replaceAll("NAME", "Device" + controlDeviceCount + dg.opInfo.getString("name"));
-                            dg.mintName = "Device" + flowDeviceCount + dg.opInfo.getString("name");
-                            
-                            for (DWire in:dg.input)
-                            {
-                                if (in.isWritten == false)
-                                {
-                                    
-                                    in.isWritten = true;
-                                }
-                            }
-                            
-                            if (dg.output.isWritten == false)                            
-                            {
-                                
-                                dg.output.isWritten = true;
-                            }
-                            
-                            controlDevices+=cDevLine;
-                            controlDevices+="\n";
+                            cDevLine = cDevLine.replaceAll("NAME", "cDevice" + controlDeviceCount + "_" + dg.opInfo.getString("name"));
+                            dg.mintName = "cDevice" + controlDeviceCount + "_" + dg.opInfo.getString("name");
+
+                            controlDevices += cDevLine;
+                            controlDevices += "\n";
                             dg.isWritten = true;
                             controlDeviceCount++;
                             break;
-                        
-                        default: System.out.println("unlayered gate! UCF/Bug?"); break;
+
+                        default:
+                            System.out.println("unlayered gate! UCF/Bug?");
+                            break;
                     }
                     break;
-                    
-                case uF_IN:
-                    switch (dg.layer) 
-                    {
-                        case flow:
+
+                case uF_IN:                                         //inport
+                    switch (dg.layer) {
+                        case flow:                                  //flow inport
                             flowInPorts += "flowInPort" + flowInPortCount + ",";
                             dg.mintName = "flowInPort" + flowInPortCount;
-                            
-                            if (dg.output.isWritten == false)
-                            {
-                                
-                                dg.output.isWritten = true;
-                            }
-                            
+
                             dg.isWritten = true;
                             flowInPortCount++;
                             break;
-                        
-                        case control:
-                            controlInPorts += "controlInPort"+controlInPortCount+",";
-                            dg.mintName = "controlInPort"+controlInPortCount;
-                            
-                            if (dg.output.isWritten == false)
-                            {
-                                
-                                dg.output.isWritten = true;
-                            }
-                            
+
+                        case control:                               //control inport
+                            controlInPorts += "controlInPort" + controlInPortCount + ",";
+                            dg.mintName = "controlInPort" + controlInPortCount;
+
                             dg.isWritten = true;
                             controlInPortCount++;
                             break;
-                        
-                        default: System.out.println("unlayered gate! UCF/Bug?"); break; 
+
+                        default:
+                            System.out.println("unlayered gate! UCF/Bug?");
+                            break;
                     }
                     break;
-                    
+
                 case uF_OUT:
-                    switch (dg.layer) 
-                    {
+                    switch (dg.layer) {
                         case flow:
-                            flowOutPorts+="outPort"+flowOutPortCount+",";
-                            dg.mintName = "outPort"+flowOutPortCount;
-                            
-                            if (dg.input.get(0).isWritten == false)
-                            {
-                                
-                                dg.input.get(0).isWritten = true;
-                            }
-                            
+                            flowOutPorts += "outPort" + flowOutPortCount + ",";
+                            dg.mintName = "outPort" + flowOutPortCount;
+
                             dg.isWritten = true;
                             flowOutPortCount++;
                             break;
                         case control:
-                           controlOutPorts+="outPort"+controlOutPortCount+",";
-                            dg.mintName = "outPort"+controlOutPortCount;
-                            
-                            if (dg.input.get(0).isWritten == false)
-                            {
-                                
-                                dg.input.get(0).isWritten = true;
-                            }
-                                                        
+                            controlOutPorts += "outPort" + controlOutPortCount + ",";
+                            dg.mintName = "outPort" + controlOutPortCount;
+
                             dg.isWritten = true;
-                            controlOutPortCount++; 
+                            controlOutPortCount++;
                             break;
 
-                        default: System.out.println("unlayered gate! UCF/Bug?"); break;
+                        default:
+                            System.out.println("unlayered gate! UCF/Bug?");
+                            break;
                     }
                     break;
-                    
+
                 default:
-                    System.out.println("Untyped gate! Netsynth bug?"); break;
+                    System.out.println("Untyped gate! Netsynth bug?");
+                    break;
             }
         }
-        flowPorts = flowInPorts+flowOutPorts;
-        flowPorts = flowPorts.substring(0, flowPorts.length()-1);   //removing extra comma
-        
-        controlPorts = controlInPorts+controlOutPorts;
-        controlPorts = controlPorts.substring(0, controlPorts.length()-1); //removing extra comma
-        
-        printMint(fileName);
-                 
-        //adding channels
-//        String controlChannels = "";
-        for(MuWire w : graph.wires)                 //printing flow channels eg: "CHANNEL flowchannel0 from Device0 2 to Device1 4 w=100;"
-        {
-            if (w.isWritten == true) continue;      //skip any duplicate channels
-            else if (w.dupChannel != null) w.setDestination(w.dupChannel.toGate);   //combining the duplicate channel from any valve-gates
-                
-            int currInTerm;                         //checking which terminal to use for channel printing
-            int currOutTerm;        
-            if (w.fromGate.outTermFlag == true)
-            {
-                currOutTerm = w.fromGate.opInfo.getInt("outputTerms");
-            }
-            else currOutTerm = w.fromGate.outTermVal;
-            
-            if (w.toGate.inTermFlag == true)
-            {                
-                JSONArray inTermsArray = w.toGate.opInfo.getJSONArray("inputTerms");
-                currInTerm = inTermsArray.getInt(w.toGate.inTermInd);
-                w.toGate.inTermInd++;       //incrementing the gate's in terminal index
-            }
-            else currInTerm = w.toGate.inTermVal;
-                        
-            if (w.type.equals("fchan") || w.type.equals("finput"))
-            {
-                String channelMintLine = "CHANNEL flowchannel"+channelCount+" from ";   //adding "CHANNEL flowchannel0 from " to line
-                channelMintLine += w.fromGate.mintName+" ";                         //adding "Device0 " to line
-                channelMintLine += currOutTerm+" to ";                               //adding "2 to " to line
-                channelMintLine += w.toGate.mintName+" ";                           //adding  "Device1 " to line
-                channelMintLine += currInTerm+" w=100;";                             //adding "4 w=100;" to line
-                mintWriter.println(channelMintLine);                                //printing whole line
-                w.isWritten = true;                                                 //marking wire as printed
-                channelCount++;
-            }
-            else if (w.type.equals("cchan") || w.type.equals("cinput"))
-            {
-                controlChannels += "\nCHANNEL controlchannel"+channelCount+" from ";   //adding "CHANNEL flowchannel0 from " to line
-                controlChannels += w.fromGate.mintName+" ";                         //adding "Device0 " to line
-                controlChannels += currOutTerm+" to ";                               //adding "2 to " to line
-                controlChannels += w.toGate.mintName+" ";                           //adding  "Device1 " to line
-                controlChannels += currInTerm+" w=100;\n";                             //adding "4 w=100;" to line
-                w.isWritten = true;                                                 //marking wire as printed
-                channelCount++;  
-            }
-        }                       //TODO: NEED TO MAKE CHANNEL SIZE PARAMETRIC ^^^ <---GUI controlled sizing?
-        
 
+        for (DWire dw : graph.wireGraph) {
+            int currentInTerm;
+            int currentOutTerm;
+            JSONArray inTermsArray;
+            if (dw.isWritten == false) {
+                switch (dw.wtype) {
+                    case cinput:
+                        //inTermsArray = dw.toGate.opInfo.getJSONArray("inputTerms");     //need to be catch all with if inTermsFlag
+
+                        currentInTerm = dw.toGate.opInfo.getInt("controlTerms");
+
+                        controlChannels += "CHANNEL controlChannel" + controlChannelCount + " from ";
+                        controlChannels += dw.fromGate.mintName + " " + dw.fromGate.outTermVal + " to ";
+                        controlChannels += dw.toGate.mintName + " " + currentInTerm + " w=" + channelWidth + ";\n";
+                        dw.isWritten = true;
+                        controlChannelCount++;
+
+                        break;
+
+                    case finput:
+                        if (dw.toGate.layer.equals(LayerType.control)) {
+                            currentInTerm = dw.toGate.opInfo.getInt("inputTerms");
+                        } else {
+                            inTermsArray = dw.toGate.opInfo.getJSONArray("inputTerms");
+                            currentInTerm = inTermsArray.getInt(dw.toGate.inTermInd);
+                            dw.toGate.inTermInd++;
+                        }
+
+                        flowChannels += "CHANNEL flowChannel" + flowChannelCount + " from ";
+                        flowChannels += dw.fromGate.mintName + " " + dw.fromGate.outTermVal + " to ";
+                        flowChannels += dw.toGate.mintName + " " + currentInTerm + " w=" + channelWidth + ";\n";
+                        dw.isWritten = true;
+                        flowChannelCount++;
+
+                        break;
+
+                    case coutput:
+                        currentInTerm = dw.toGate.inTermVal;
+                        currentOutTerm = dw.fromGate.opInfo.getInt("outPutTerms");     //need to be catch all with outTermFlag, be done with JSON parsing by translation, store terms int object attribute?
+
+                        controlChannels += "CHANNEL controlChannel" + controlChannelCount + " from ";
+                        controlChannels += dw.fromGate.mintName + " " + currentOutTerm + " to ";
+                        controlChannels += dw.toGate.mintName + " " + currentInTerm + " w=" + channelWidth + ";\n";
+                        dw.isWritten = true;
+                        controlChannelCount++;
+
+                        break;
+
+                    case foutput:
+                        currentInTerm = dw.toGate.inTermVal;
+                        currentOutTerm = dw.fromGate.opInfo.getInt("outputTerms");     //need to be catch all with outTermFlag, be done with JSON parsing by translation, store terms int object attribute?
+
+                        flowChannels += "CHANNEL flowChannel" + flowChannelCount + " from ";
+                        flowChannels += dw.fromGate.mintName + " " + currentOutTerm + " to ";
+                        flowChannels += dw.toGate.mintName + " " + currentInTerm + " w=" + channelWidth + ";\n";
+                        dw.isWritten = true;
+                        flowChannelCount++;
+
+                        break;
+
+                    case cchannel:
+                        //to be inplemented
+                        break;
+
+                    case fchannel:
+                        inTermsArray = dw.toGate.opInfo.getJSONArray("inputTerms");     //need to be catch all with if inTermsFlag
+                        currentInTerm = inTermsArray.getInt(dw.toGate.inTermInd);
+                        dw.toGate.inTermInd++;
+
+                        currentOutTerm = dw.fromGate.opInfo.getInt("outputTerms");
+
+                        flowChannels += "CHANNEL flowChannel" + flowChannelCount + " from ";
+                        flowChannels += dw.fromGate.mintName + " " + currentOutTerm + " to ";
+                        flowChannels += dw.toGate.mintName + " " + currentInTerm + " w=" + channelWidth + ";\n";
+                        dw.isWritten = true;
+                        flowChannelCount++;
+
+                        break;
+
+                    default:
+                        System.out.println("untyped wire! check netsynth!");
+                        break;
+                }
+            }
+        }
+        flowPorts = flowInPorts + flowOutPorts;
+        flowPorts = flowPorts.substring(0, flowPorts.length() - 1);   //removing extra comma
+
+        controlPorts = controlInPorts + controlOutPorts;
+        controlPorts = controlPorts.substring(0, controlPorts.length() - 1); //removing extra comma
+
+        printMint(fileName);
     }
-    public void printMint(String fileName) throws FileNotFoundException, UnsupportedEncodingException
-    {
+
+    public void printMint(String fileName) throws FileNotFoundException, UnsupportedEncodingException {
         PrintWriter mintWriter = new PrintWriter(fileName, "UTF-8");
         mintWriter.println("# .uf output by muShroomMapper");
-        mintWriter.println("DEVICE testDevice");
+        mintWriter.println("3D DEVICE testDevice");
         mintWriter.println("");
-        
-        mintWriter.println("LAYER FLOW");
-        mintWriter.println("");
-        
-        mintWriter.println("PORT " + flowPorts + " r=" + portRadius);
+
+        mintWriter.println("LAYER FLOW\n\n");
+
+        mintWriter.println("PORT " + flowPorts + " r=" + portRadius + ";\n");
         mintWriter.println(flowDevices);
+        mintWriter.println(controlDevices); //because 3dvalve is bugged in fluigi
         mintWriter.println(flowChannels);
-        
+
         mintWriter.println("");
         mintWriter.println("END LAYER");
         mintWriter.println("");
-        
-        mintWriter.println("LAYER CONTROL");
-        mintWriter.println("");
-        
-        mintWriter.println("PORT " + controlPorts + " r=" + portRadius);
-        mintWriter.println(controlDevices);
+
+        mintWriter.println("LAYER CONTROL\n\n");
+
+        mintWriter.println("PORT " + controlPorts + " r=" + portRadius + ";\n");
+        // mintWriter.println(controlDevices); //because 3dvalve is bugged in fluigi
         mintWriter.println(controlChannels);
-        
+
         mintWriter.println("");
         mintWriter.println("END LAYER");
-        
+
         mintWriter.close();
     }
-    
+
 }
