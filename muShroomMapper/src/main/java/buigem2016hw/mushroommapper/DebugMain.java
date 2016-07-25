9a56fe6d8fb51ac6359e66de4bbcd0490ff97c56
aonanam <aonanam@bu.edu>
2016-07-25 10:51:10 -0400

    Alt main used for debugging

---
+++ muShroomMapper/src/main/java/buigem2016hw/mushroommapper/DebugMain.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buigem2016hw.mushroommapper;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import static org.cidarlab.fluigi.fluigi.PiranhaPlanner.processMintDevice;
+import org.json.JSONException;
+
+/**
+ *
+ * @author Shane
+ */
+public class DebugMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException, JSONException, IOException {
+               //Read in ucf
+            Scanner reader = new Scanner(System.in);  // Reading from System.in
+            System.out.println("Enter path/to/UCFFile: ");
+            //String ucfPath = reader.nextLine(); // reads in filepath
+            String ucfPath = "ucf.json";     //temp path for debugging
+            System.out.println("Reading UCF...");
+            ParsedUCF ucf = new ParsedUCF(ucfPath);
+
+        //Read Verilog + Make Graph
+            //System.out.println("Enter path/to/VerilogFile: ");
+            //String vPath = reader.nextLine(); // reads in filepath
+            String vPath = "lfr.v";          //temp path for debugging
+            System.out.println("Reading Verilog...");
+            NetListTransition nlt = new NetListTransition(ucf, vPath);
+
+        //Create JGraphX from netlist Graph while error checking
+            /*
+            System.out.println("Creating JGraphX, error checking...");
+            GraphTranslation gt = new GraphTranslation(nlt, ucf);
+
+        //create a visualization using JGraphX
+            Visualization v = new Visualization();
+            v.display(gt.jgraphx);
+            */
+        
+        //Create Mint file from netlist graph and parsed ucf
+            System.out.println("Creating Mint file output...");
+            //Scanner ufNameInput = new Scanner(System.in);  //Reading from System.in
+            //System.out.println("What would you like to name your .uf file? ");
+            //String fileName = ufNameInput.nextLine();
+            String fileName = "testMINT.uf";
+            CreateMint cm = new CreateMint(nlt, ucf, fileName, 100);
+            processMintDevice(fileName, "SampleInput/fluigi.ini", "sej", true);
+            System.out.println("MM is all done!");
+            System.exit(0);
+    }
+    
+}
