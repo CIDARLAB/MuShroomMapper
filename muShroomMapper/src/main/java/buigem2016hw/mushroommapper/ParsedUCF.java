3f33667cdebc4b713b1b4faefa9a4f031c808c25
Shane McCormack <aonanam@bu.edu>
2017-03-31 11:51:13 -0400

    Addition of (basic) Vulcan implementation on UCF

--- muShroomMapper/src/main/java/buigem2016hw/mushroommapper/ParsedUCF.java
+++ muShroomMapper/src/main/java/buigem2016hw/mushroommapper/ParsedUCF.java
@@ -7,9 +7,13 @@ package buigem2016hw.mushroommapper;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import static org.cidarlab.fluigi.fluigi.PiranhaPlanner.processMintDevice;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,7 +30,7 @@ public class ParsedUCF {
     public int portRadius;
     public boolean autoSplitMerge;
     
-    ParsedUCF(String ucfPath) throws FileNotFoundException, JSONException
+    ParsedUCF(String ucfPath) throws FileNotFoundException, JSONException, UnsupportedEncodingException, IOException
     {
         String ucfString = new Scanner(new File(ucfPath)).useDelimiter("\\Z").next();   //take in file as string
         JSONArray opArray = new JSONArray(ucfString);                                   //make string a JSONArray
@@ -39,6 +43,26 @@ public class ParsedUCF {
                 portRadius = opObj.getInt("portRadius");
                 autoSplitMerge = opObj.getBoolean("automatedSplitMerge");
                 continue;   //moving to next block
+            }
+            if (n == 1)                                             //vulcan hw info block
+            {
+                JSONObject opObj = opArray.getJSONObject(n);
+                if(opObj.getBoolean("Are you using Vulcan?") == true)
+                {
+                    JSONObject pcrObject = opObj.getJSONObject("Vulcan Hardware").getJSONObject("PCR");
+                    int numBends = pcrObject.getInt("Number of bends (1 bend = 1 'period')");
+                    int bendSpacing = pcrObject.getInt("Spacing between bends (um)");
+                    int bendLength = pcrObject.getInt("Length of each bend (um)");
+                    int channelWidth = pcrObject.getInt("Width of channels");
+                    
+                    String pcrMINTString = "3D DEVICE integratedPCR\n" + "\n" + "LAYER FLOW\n" + "\n" + "PORT flowInPort0,outPort0 r=2020;\n" + "\n" + "H MIXER pcrMixer numBends="+numBends+" bendSpacing="+bendSpacing+" bendLength="+bendLength+" channelWidth="+channelWidth+";\n" + "\n" + "CHANNEL flowChannel0 from flowInPort0 2 to pcrMixer 1 w="+channelWidth+";\n" +
+"CHANNEL flowChannel1 from pcrMixer 2 to outPort0 4 w="+channelWidth+";\n" + "\n" + "END LAYER\n" + "\n" + "LAYER CONTROL\n" + "\n" + "END LAYER";
+                    PrintWriter mintWriter = new PrintWriter("output/pcr.uf", "UTF-8");
+                    mintWriter.println(pcrMINTString);
+                    mintWriter.close();
+                    processMintDevice("output/pcr.uf", "fluigi.ini", "sej", true);                                       
+                }
+                continue;
             }
             JSONObject opObj = opArray.getJSONObject(n);        //grabbing JSONObject                               
             String operator = opObj.getString("operator");      //grabbing operator attribute of JSONObject
